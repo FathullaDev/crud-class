af35b26c5d1ee0292f0894bc628ae9bb9c972e6f
Fathulla <02fathu@gmail.com>
2025-11-29 19:29:48 +0500

    updated

--- app/urls.py
+++ app/urls.py
@@ -12,61 +12,42 @@ from app.views import *
 # ]
 
 urlpatterns = [
-    path('', index, name='home'),
-    path('otm/<int:otm_id>/groups/', groups_by_otm, name='groups_by_otm'),
-    path('otm/<int:otm_id>/groups/<int:group_id>/students/', students_by_groups_and_otm, name='students_by_groups_and_otm'),
-    path('student/<int:student_id>/', view_student, name='view_student'),
-
-    path('add_otm/',add_otm,name="add_otm"),
-    path('add_group/',add_group,name="add_group"),
-    path('add_student/',add_student,name="add_student"),
-
-    path('update_otm/<int:otm_id>/', update_otm, name='update_otm'),
-    path('update_group/<int:group_id>/', update_group, name='update_group'),
-    path('update_student/<int:student_id>/', update_student, name='update_student'),
-
-    path('delete_otm/<int:otm_id>/', delete_otm, name="delete_otm"),
-    path('delete_group/<int:group_id>/', delete_group, name="delete_group"),
-    path('delete_student/<int:student_id>/', delete_student, name="delete_student"),
-
-
+    # path('', index, name='home'),
+    # path('otm/<int:otm_id>/groups/', groups_by_otm, name='groups_by_otm'),
+    # path('otm/<int:otm_id>/groups/<int:group_id>/students/', students_by_groups_and_otm, name='students_by_groups_and_otm'),
+    # path('student/<int:student_id>/', view_student, name='view_student'),
+    #
+    # path('add_otm/',add_otm,name="add_otm"),
+    # path('add_group/',add_group,name="add_group"),
+    # path('add_student/',add_student,name="add_student"),
+    #
+    # path('update_otm/<int:otm_id>/', update_otm, name='update_otm'),
+    # path('update_group/<int:group_id>/', update_group, name='update_group'),
+    # path('update_student/<int:student_id>/', update_student, name='update_student'),
+    #
+    # path('delete_otm/<int:otm_id>/', delete_otm, name="delete_otm"),
+    # path('delete_group/<int:group_id>/', delete_group, name="delete_group"),
+    # path('delete_student/<int:student_id>/', delete_student, name="delete_student"),
 
 
-# path('', views.index, name='home'),
-#
-#     path('otm/<int:otm_id>/groups/', views.groups_by_otm, name='groups_by_otm'),
-#     path('otm/<int:otm_id>/groups/<int:group_id>/students/',
-#          views.students_by_groups_and_otm,
-#          name='students_by_groups_and_otm'),
-#
-#     # Create (CBV)
-#     path('add_otm/', OtmCreateView.as_view(), name='add_otm'),
-#     path('add_group/', GroupCreateView.as_view(), name='add_group'),
-#     path('add_student/', StudentCreateView.as_view(), name='add_student'),
-#
-#     # Update (CBV)
-#     path('update_otm/<int:otm_id>/',
-#          OtmUpdateView.as_view(),
-#          name='update_otm'),
-#
-#     path('update_group/<int:group_id>/',
-#          GroupUpdateView.as_view(),
-#          name='update_group'),
-#
-#     path('update_student/<int:student_id>/',
-#          StudentUpdateView.as_view(),
-#          name='update_student'),
-#
-#     # Delete (CBV)
-#     path('delete_otm/<int:otm_id>/',
-#          OtmDeleteView.as_view(),
-#          name='delete_otm'),
-#
-#     path('delete_group/<int:group_id>/',
-#          GroupDeleteView.as_view(),
-#          name='delete_group'),
-#
-#     path('delete_student/<int:student_id>/',
-#          StudentDeleteView.as_view(),
-#          name='delete_student'),
+    path('', OtmListView.as_view(), name='home'),
+    path('otm/<int:otm_id>/groups/', GroupsByOtmView.as_view(), name='groups_by_otm'),
+    path('otm/<int:otm_id>/groups/<int:group_id>/students/',StudentsByGroupAndOtmView.as_view(), name='students_by_groups_and_otm'),
+    path('student/<int:student_id>/', StudentDetailView.as_view(), name='view_student'),
+
+    # add
+    path('add_otm/', AddOtmView.as_view(), name='add_otm'),
+    path('add_group/', AddGroupView.as_view(), name='add_group'),
+    path('add_student/', AddStudentView.as_view(), name='add_student'),
+
+    # update
+    path('update_otm/<int:otm_id>/', UpdateOtmView.as_view(), name='update_otm'),
+    path('update_group/<int:group_id>/', UpdateGroupView.as_view(), name='update_group'),
+    path('update_student/<int:student_id>/', UpdateStudentView.as_view(), name='update_student'),
+
+    # delete
+    path('delete_otm/<int:otm_id>/', DeleteOtmView.as_view(), name='delete_otm'),
+    path('delete_group/<int:group_id>/', DeleteGroupView.as_view(), name='delete_group'),
+    path('delete_student/<int:student_id>/', DeleteStudentView.as_view(), name='delete_student'),
+
 ]
--- app/views.py
+++ app/views.py
@@ -2,12 +2,23 @@ from django.shortcuts import render
 from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse_lazy
 from django.views.generic import *
-
 from app.forms import *
 from app.models import *
+from django.views.generic import (
+    ListView, DetailView, CreateView, UpdateView, DeleteView
+)
+
+
 def index(request):
     otm = Otm.objects.all()
     return render(request, 'index.html', {'otm': otm})
+
+class OtmListView(ListView):
+    model = Otm
+    template_name = 'index.html'
+    context_object_name = 'otm'
+
+
 
 def groups_by_otm(request, otm_id):
     selected_otm = get_object_or_404(Otm, id=otm_id)
@@ -19,6 +30,19 @@ def groups_by_otm(request, otm_id):
         'all_otm': all_otm,
         'groups_for_selected': groups_for_selected,
     })
+
+class GroupsByOtmView(ListView):
+    model = Otm
+    template_name = 'groups.html'
+    context_object_name = 'all_otm'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        otm_id = self.kwargs['otm_id']
+
+        context['selected_otm'] = get_object_or_404(Otm, id=otm_id)
+        context['groups_for_selected'] = Groups.objects.filter(otm_id=otm_id)
+        return context
 
 
 def students_by_groups_and_otm(request, otm_id, group_id):
@@ -33,9 +57,40 @@ def students_by_groups_and_otm(request, otm_id, group_id):
         'all_otm': all_otm,
     })
 
+class StudentsByGroupAndOtmView(ListView):
+    model = Students
+    template_name = 'students.html'
+    context_object_name = 'students'
+
+    def get_queryset(self):
+        group = get_object_or_404(
+            Groups,
+            id=self.kwargs['group_id'],
+            otm_id=self.kwargs['otm_id']
+        )
+        return Students.objects.filter(group=group).order_by('id')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        otm_id = self.kwargs['otm_id']
+        group_id = self.kwargs['group_id']
+
+        context['otm'] = get_object_or_404(Otm, id=otm_id)
+        context['group'] = get_object_or_404(Groups, id=group_id)
+        context['all_otm'] = Otm.objects.all()
+        return context
+
+
 def view_student(request, student_id):
     student = get_object_or_404(Students, id=student_id)
     return render(request, 'view_student.html', {'student': student})
+
+class StudentDetailView(DetailView):
+    model = Students
+    template_name = 'view_student.html'
+    context_object_name = 'student'
+    pk_url_kwarg = 'student_id'
 
 
 #add functions
@@ -49,6 +104,13 @@ def add_otm(request):
         form=OtmForm()
     return render(request,'add_otm.html',{'form':form})
 
+class AddOtmView(CreateView):
+    model = Otm
+    form_class = OtmForm
+    template_name = 'add_otm.html'
+    success_url = reverse_lazy('home')
+
+
 def add_group(request):
     if request.method=='POST':
         form=GroupForm(request.POST,request.FILES)
@@ -59,6 +121,14 @@ def add_group(request):
         form=GroupForm()
     return render(request,'add_group.html',{'form':form})
 
+class AddGroupView(CreateView):
+    model = Groups
+    form_class = GroupForm
+    template_name = 'add_group.html'
+    success_url = reverse_lazy('home')
+
+
+
 def add_student(request):
     if request.method=='POST':
         form=StudentForm(request.POST,request.FILES)
@@ -68,6 +138,12 @@ def add_student(request):
     else:
         form=StudentForm()
     return render(request,'add_student.html',{'form':form})
+
+class AddStudentView(CreateView):
+    model = Students
+    form_class = StudentForm
+    template_name = 'add_student.html'
+    success_url = reverse_lazy('home')
 
 
 #update functions
@@ -84,6 +160,14 @@ def update_otm(request,otm_id):
 
     return render(request,'update_otm.html',{'form': form, 'otm': otm})
 
+class UpdateOtmView(UpdateView):
+    model = Otm
+    form_class = OtmForm
+    template_name = 'update_otm.html'
+    pk_url_kwarg = 'otm_id'
+    success_url = reverse_lazy('home')
+
+
 
 def update_group(request,group_id):
     group = get_object_or_404(Groups, pk=group_id)
@@ -97,6 +181,14 @@ def update_group(request,group_id):
         form = GroupForm(instance=group)
 
     return render(request, 'update_group.html',{'form': form, 'group': group})
+
+class UpdateGroupView(UpdateView):
+    model = Groups
+    form_class = GroupForm
+    template_name = 'update_group.html'
+    pk_url_kwarg = 'group_id'
+    success_url = reverse_lazy('home')
+
 
 
 def update_student(request,student_id):
@@ -112,6 +204,12 @@ def update_student(request,student_id):
 
     return render(request, 'update_student.html', {'form': form, 'student': student})
 
+class UpdateStudentView(UpdateView):
+    model = Students
+    form_class = StudentForm
+    template_name = 'update_student.html'
+    pk_url_kwarg = 'student_id'
+    success_url = reverse_lazy('home')
 
 
 # #delete functions
@@ -120,12 +218,33 @@ def delete_otm(request,otm_id):
     otm.delete()
     return redirect('home')
 
+class DeleteOtmView(DeleteView):
+    model = Otm
+    pk_url_kwarg = 'otm_id'
+    template_name = 'confirm_delete.html'
+    success_url = reverse_lazy('home')
+
+
 def delete_group(request,group_id):
     group=get_object_or_404(Groups,pk=group_id)
     group.delete()
     return redirect('home')
 
+class DeleteGroupView(DeleteView):
+    model = Groups
+    pk_url_kwarg = 'group_id'
+    template_name = 'confirm_delete.html'
+    success_url = reverse_lazy('home')
+
+
+
 def delete_student(request,student_id):
     student=get_object_or_404(Students,pk=student_id)
     student.delete()
     return redirect('home')
+
+class DeleteStudentView(DeleteView):
+    model = Students
+    pk_url_kwarg = 'student_id'
+    template_name = 'confirm_delete.html'
+    success_url = reverse_lazy('home')
